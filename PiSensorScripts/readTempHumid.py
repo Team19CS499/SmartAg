b7ded9a46ad5ea33b95feab066fbbc6674405966
Team19CS499 <36999992+Team19CS499@users.noreply.github.com>
2018-04-19 00:30:42 -0400

    Renamed, changed to write data to files

--- PiSensorScripts/readTempHumid.py
+++ PiSensorScripts/readTempHumid.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python
+# Copyright (c) 2014 Adafruit Industries
+# Author: Tony DiCola
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
 
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -11,6 +23,8 @@ import sys
 
 import Adafruit_DHT
 
+tempFile = "temperature_data.txt"
+humidFile = "humidity_data.txt"
 
 # Parse command line parameters.
 sensor_args = { '11': Adafruit_DHT.DHT11,
@@ -20,13 +34,14 @@ if len(sys.argv) == 3 and sys.argv[1] in sensor_args:
     sensor = sensor_args[sys.argv[1]]
     pin = sys.argv[2]
 else:
-    print('usage: sudo ./Adafruit_DHT.py [11|22|2302] GPIOpin#')
-    print('example: sudo ./Adafruit_DHT.py 2302 4 - Read from an AM2302 connected to GPIO #4')
+    print("usage: sudo ./Adafruit_DHT.py [11|22|2302] GPIOpin#")
+    print("example: sudo ./Adafruit_DHT.py 2302 4 - Read from an AM2302 connected to GPIO #4")
     sys.exit(1)
 
 # Try to grab a sensor reading.  Use the read_retry method which will retry up
 # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
-humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+#print( Adafruit_DHT.read_retry(11, 23))
+humidity, temperature = Adafruit_DHT.read_retry(11, 23)
 
 # Un-comment the line below to convert the temperature to Fahrenheit.
 # temperature = temperature * 9/5.0 + 32
@@ -36,7 +51,13 @@ humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 # guarantee the timing of calls to read the sensor).
 # If this happens try again!
 if humidity is not None and temperature is not None:
-    print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
+    f = open(tempFile,"w")
+    f.write(str(temperature))
+
+    humidF = open(humidFile,"w")
+    humidF.write(str(humidity))
+
+#    print("Temp={0:0.1f}*  Humidity={1:0.1f}%".format(temperature, humidity))
 else:
     print('Failed to get reading. Try again!')
-sys.exit(1)
+    sys.exit(1)
