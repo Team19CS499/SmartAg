85f304ea328b3da21fbeb1d2b2ba46ac28b50091
Team19CS499 <36999992+Team19CS499@users.noreply.github.com>
2018-05-02 23:05:35 -0400

    Update led_off.sh

--- PiSensorScripts/led_off.sh
+++ PiSensorScripts/led_off.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+
+# This script uses the WiringPi library to turn the light in the system off
+# This program is being used by team 19 in CS499 at the University of Kentucky. Members include Austin Vanderpool, Delbert Harrison, Jesse Vaught, Steven Liu. 
+# Written by Austin Vanderpool
+
 gpio -g write 24 0
