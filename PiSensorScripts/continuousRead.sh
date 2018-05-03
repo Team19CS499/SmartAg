2576c2e18e1740f58072fe353c78178e2654fd4c
Team19CS499 <36999992+Team19CS499@users.noreply.github.com>
2018-05-02 23:04:48 -0400

    Update continuousRead.sh

--- PiSensorScripts/continuousRead.sh
+++ PiSensorScripts/continuousRead.sh
@@ -1,9 +1,12 @@
 #! /bin/bash
+# This script continuously runs the python programs that gather the sensor data. It sleeps for 30 seconds because our data does not need to be that up to date.
+# This program is being used by team 19 in CS499 at the University of Kentucky. Members include Austin Vanderpool, Delbert Harrison, Jesse Vaught, Steven Liu. 
+# Written by Austin Vanderpool
 
 while [ 1 ]
 do
-    python readLight.py
-    python readMoisture.py
-    python readTempHumid.py 11 23
-    sleep 10
+    python readLight.py 
+    python readMoisture.py 
+    python readTempHumid.py 11 23 
+    sleep 300 
 done
